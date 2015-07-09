921e70202c55242d7ac263922851fd7d91740415
Hlkz <hlkz@hotmail.fr>
2015-07-09 17:49:52 +0200

    Up

--- Update/clean.bat
+++ Update/clean.bat
@@ -5,18 +5,20 @@ mkdir data
 rd frFR /Q /S
 rd enUS /Q /S
 
-del res\data\patch.cmd /Q
-del res\data\patch.html /Q
+rd installers /Q /S
+rd downloaders /Q /S
+del *.torrent /Q
+for /d %%G in ("wow-*.exe") do rd /s /q "%%~G"
+
 rd data_tmp /Q /S
 del *.mpq /Q
 
-for /d %%G in ("wow-*.exe") do rd /s /q "%%~G"
-
-rd installers /Q /S
-rd downloaders /Q /S
-
-del *.torrent /Q
 del prepatch.lst /Q
 del base.exe /Q
+del res\Installer.exe /Q
+del res\skin-installer.mpq /Q
+del res\skin-downloader.mpq /Q
+del res\data\patch.cmd /Q
+del res\data\patch.html /Q
 
 ::pause
